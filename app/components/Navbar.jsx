8f4ef677425d6b07e2531f360a3813794a294a53
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-04-26 19:05:13 -0700

    added more parts to the navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useState } from 'react'
 import { GithubIcon, LinkedInIcon } from './Icons'
+import { motion } from "framer-motion"
 
 const Navbar = () => {
 
@@ -32,11 +33,11 @@ const Navbar = () => {
         }
     ]
   return (
-    <div className='w-full h-full md:px-32 px-8 flex justify-center items-center'>
+    <div className='w-full h-full md:px-32 px-8 flex justify-center items-center relative'>
       <button onClick={() => handleClick()} className='flex md:hidden flex-col gap-1 justify-center items-center'>
-        <span className={`w-6 h-0.5 bg-black ${open ? 'rotate-45 translate-y-0.5' : ''}`}></span>
-        <span className={`w-6 h-0.5 bg-black ${open ? 'hidden' : ''}`}></span>
-        <span className={`w-6 h-0.5 bg-black ${open ? '-rotate-45 -translate-y-1' : ''}`}></span>
+        <span className={`w-6 h-0.5 bg-black ease-in duration-200 ${open ? 'rotate-45 translate-y-0.5' : ''}`}></span>
+        <span className={`w-6 h-0.5 bg-black ease-in duration-200 ${open ? 'hidden' : ''}`}></span>
+        <span className={`w-6 h-0.5 bg-black ease-in duration-200 ${open ? '-rotate-45 -translate-y-1' : ''}`}></span>
       </button>
       <div className='w-full h-full flex items-center justify-evenly'>
         <nav className='md:flex hidden gap-6 w-1/3'>
@@ -60,6 +61,30 @@ const Navbar = () => {
           </Link> */}
         </nav>
       </div>
+
+      {open && 
+        <motion.div initial={{scale:0, opacity:0, x:"-50%", y:"-50%"}}
+        animate={{scale:1, opacity:1}} className='w-[70vw] z-30 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 fixed bg-black/90 opacity-80 h-3/4 rounded-lg backdrop-blur-xl'>
+          <nav className=' h-full flex flex-col items-center justify-center gap-4'>
+            {links.map(({id, title, path}) => (
+              <Link className='text-light group relative' key={id} href={path}>
+                {title}
+                <span className={`absolute h-[2px] group-hover:w-full ease-in duration-200 bg-light top-5 left-0 ${pathname === path ? 'w-full' : 'w-0'}`}></span>
+              </Link>
+            ))}
+            <nav className='w-22 gap-4 flex'>
+              <Link className='w-8 h-8' href="/">
+                <GithubIcon className={`bg-light`}/>
+              </Link>
+              <Link className='w-8 h-8' href="/">
+                <LinkedInIcon/>
+              </Link>
+            </nav>
+          </nav>
+
+          
+        </motion.div>
+      }
       
       
       
