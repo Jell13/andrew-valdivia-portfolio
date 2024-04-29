82f44bafa8fa4a54dee5afa99aac1b2c541ff599
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-04-29 13:44:56 -0700

    fixed the margin problem in the about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -9,7 +9,7 @@ const About = () => {
     <>
       <TransitionEffect/>
       <div className='w-full h-full px-20 flex flex-col items-center justify-center'>
-        <h1 className='md:text-6xl mt-8 md:mt-8 text-3xl md:mb-28 font-semibold'>Quote Goes Here</h1>
+        <h1 className='md:text-6xl mt-8 md:mt-20 text-3xl md:mb-2 font-semibold'>Quote Goes Here</h1>
         <div className='flex w-full flex-col md:flex-row h-full justify-center items-center gap-6'>
           <div className='md:w-1/4 w-1/2 mt-4 border-2 border-black dark:border-light rounded-2xl relative'>
             <Image className='w-full h-auto rounded-2xl' src="/about.jpg" width={100} height={100}/>
