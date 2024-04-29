9164d6eb8931d24551f17ba8c51261cb13c3f8b3
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-04-28 20:26:39 -0700

    changes applied to the about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -9,13 +9,16 @@ const About = () => {
     <>
       <TransitionEffect/>
       <div className='w-full h-full px-20 flex flex-col items-center justify-center'>
-         <h1 className='text-6xl mb-4 font-semibold'>Quote Goes Here</h1>
-        <div className='flex justify-center items-center'>
-          <Image className='w-1/5' src="/about.jpg" width={100} height={100}/>
+         <h1 className='text-6xl mb-28 font-semibold'>Quote Goes Here</h1>
+        <div className='flex justify-center items-center gap-6'>
+          <div className='w-1/4 border-2 border-black dark:border-light rounded-2xl relative'>
+            <Image className='w-full h-auto rounded-2xl' src="/about.jpg" width={100} height={100}/>
+          </div>
+         
           <div className='flex flex-col w-1/2'>
             <div>
-  
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, aspernatur placeat voluptate inventore animi commodi sequi ipsam sunt, recusandae eum dolor? Nihil distinctio earum placeat quidem debitis incidunt fugiat et.</p>
+              <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, aspernatur placeat voluptate inventore animi commodi sequi ipsam sunt, recusandae eum dolor? Nihil distinctio earum placeat quidem debitis incidunt fugiat et.</p>
+              <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia itaque architecto perferendis enim dolorum minus quos minima laboriosam odio impedit accusamus, suscipit temporibus! Temporibus eveniet ipsam non corporis inventore. Modi.</p>
             </div>
           </div>
         </div>  
