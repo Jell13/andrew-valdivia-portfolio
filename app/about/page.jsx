e294bd9e67f93465ad2e28daeb2acf78f0a866f3
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-04-29 13:33:08 -0700

    fixed the about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -9,17 +9,18 @@ const About = () => {
     <>
       <TransitionEffect/>
       <div className='w-full h-full px-20 flex flex-col items-center justify-center'>
-         <h1 className='md:text-6xl mt-24 md:mt-8 text-3xl md:mb-28 font-semibold'>Quote Goes Here</h1>
-        <div className='flex w-full flex-col h-full justify-center items-center gap-6'>
-          <div className='md:w-1/4 w-1/2 border-2 border-black dark:border-light rounded-2xl relative'>
+        <h1 className='md:text-6xl mt-8 md:mt-8 text-3xl md:mb-28 font-semibold'>Quote Goes Here</h1>
+        <div className='flex w-full flex-col md:flex-row h-full justify-center items-center gap-6'>
+          <div className='md:w-1/4 w-1/2 mt-4 border-2 border-black dark:border-light rounded-2xl relative'>
             <Image className='w-full h-auto rounded-2xl' src="/about.jpg" width={100} height={100}/>
           </div>
          
-          <div className=''>
-            <div className='w-full'>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste libero molestiae cum dignissimos modi, fugit, tenetur qui non corporis repellat reiciendis quas debitis, sapiente expedita ullam. Vitae, adipisci nulla! Saepe.</p>
-            </div>
+          
+          <div className='md:w-1/2'>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste libero molestiae cum dignissimos modi, fugit, tenetur qui non corporis repellat reiciendis quas debitis, sapiente expedita ullam. Vitae, adipisci nulla! Saepe.</p>
+            <p className='mt-3 md:mt-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur cum recusandae similique quia necessitatibus reiciendis tenetur cupiditate repellendus, sint assumenda hic, quibusdam odio quis asperiores accusantium. Repellendus doloribus temporibus culpa.</p>
           </div>
+          
         </div>  
       </div>
     </>
